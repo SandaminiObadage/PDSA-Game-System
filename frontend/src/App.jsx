49f0f07e78db2648c6cf1bfd93c57e90079935c6
Chithmi Dilaksha <148516815+ChithmiDilaksha@users.noreply.github.com>
2026-04-20 13:04:22 +0530

    form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import Dashboard from './pages/Dashboard';
 import SixteenQueensPage from './pages/sixteen-queens/page';
 import GameNotAvailable from './pages/GameNotAvailable';
+import MinimumCostPage from './pages/minimum-cost/page';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/games/sixteen-queens" element={<SixteenQueensPage />} />
+        <Route path="/games/minimum-cost" element={<MinimumCostPage />} />
         <Route path="/games/:gameId" element={<GameNotAvailable />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
