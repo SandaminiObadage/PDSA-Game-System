6eeaa0b12da5e2271b9050cc3c2654e234156a86
Hirumi Minasha <hirumiminasha89@gmail.com>
2026-04-20 14:39:15 +0530

    add controler

---
+++ backend/src/main/java/com/nibm/pdsa/games/snakeladder/controller/SnakeLadderController.java
@@ -0,0 +1,33 @@
+package com.nibm.pdsa.games.snakeladder.controller;
+
+import com.nibm.pdsa.games.snakeladder.dto.SolveComparisonResponse;
+import com.nibm.pdsa.games.snakeladder.dto.SolveRequest;
+import com.nibm.pdsa.games.snakeladder.dto.SubmitAnswerRequest;
+import com.nibm.pdsa.games.snakeladder.dto.SubmitAnswerResponse;
+import com.nibm.pdsa.games.snakeladder.service.SnakeLadderService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/games/snake-ladder")
+public class SnakeLadderController {
+
+    private final SnakeLadderService snakeLadderService;
+
+    public SnakeLadderController(SnakeLadderService snakeLadderService) {
+        this.snakeLadderService = snakeLadderService;
+    }
+
+    @PostMapping("/solve")
+    public SolveComparisonResponse solve(@Valid @RequestBody SolveRequest request) {
+        return snakeLadderService.runComparison(request.getBoardSize());
+    }
+
+    @PostMapping("/submit")
+    public SubmitAnswerResponse submit(@Valid @RequestBody SubmitAnswerRequest request) {
+        return snakeLadderService.submitAnswer(request);
+    }
+}
